822d08d94248515bfecd25a8c367e4da025de31d
Divyansh Singh <137671160+divyansh-singh010@users.noreply.github.com>
2024-02-25 13:31:02 +0530

    index file fixed

--- ocsassignment/views.py
+++ ocsassignment/views.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
 
 
 @api_view(['GET'])
 def index(request):
-    return Response({'message': 'Bruh, why you even here? Get a job.'}, status=status.HTTP_200_OK)
+    return JsonResponse({'message': 'Bruh, why you even here? Get a job.'}, status=status.HTTP_200_OK)
